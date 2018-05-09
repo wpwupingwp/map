49fce011b0f655b8081caa9d1969badae1b5b852
Workstation <wpwupingwp@outlook.com>
2018-05-09 17:37:11 +0800

    add layers

--- draw.py
+++ draw.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from json import load
 from mpl_toolkits.basemap import Basemap
 from sys import argv
 import matplotlib.pyplot as plt
@@ -11,15 +12,18 @@ map = Basemap(epsg=south_china_sea, lat_0=xi_an[1], lon_0=xi_an[0],
               llcrnrlon=80, llcrnrlat=-2, urcrnrlon=140,
               urcrnrlat=55)
 
-with open(argv[1], 'r') as _:
-    # skip head
-    _.readline()
-    raw = _.readlines()
-data = [i.strip().split(',') for i in raw]
+data = load(open(argv[1], 'r'))
 for species in data:
     # ax.axis('off')
     plt.figure(figsize=(20, 20))
-    map.readshapefile('./province', 'province', drawbounds=True)
+    map.readshapefile('/home/ping/work/map/Basemap/nation', 'nation',
+                      drawbounds=True, linewidth=2, color='#000000')
+    map.readshapefile('/home/ping/work/map/Basemap/province', 'province',
+                      drawbounds=True, linewidth=1, color='#222222')
+    map.readshapefile('/home/ping/work/map/Basemap/river', 'river',
+                      drawbounds=True, linewidth=1, color='#189EEC')
+    map.readshapefile('/home/ping/work/map/Basemap/lake', 'lake',
+                      drawbounds=True, linewidth=1, color='#189EEC')
     map.drawmeridians(range(80, 140, 10),  labels=(0, 0, 1, 1),  fontsize=15)
     map.drawparallels(range(0, 80, 10),  labels=(1, 1, 0, 0),  fontsize=15)
     loc = data[species]
@@ -31,4 +35,5 @@ for species in data:
     legend = plt.legend(bbox_to_anchor=(0.01, 0.15, 0, 0), loc='lower left',
                         fontsize=30, markerscale=5)
     # legend.get_frame().set_linewidth(0)
+    plt.show()
     plt.savefig(species+'.svg')
