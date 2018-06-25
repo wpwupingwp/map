1d5d9f8f3755b1aac74465a871ec0a9edd0dd1c7
Workstation <wpwupingwp@outlook.com>
2018-06-25 22:19:24 +0800

    add json2csv

---
+++ json2csv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from sys import argv
+import json
+
+with open(argv[1], 'r') as _:
+    data = json.load(_)
+
+with open(argv[1]+'.csv', 'w') as out:
+    for i in data:
+        for j in i:
+            out.write('{},{},{}\n'.format(i, j[0], j[1]))
