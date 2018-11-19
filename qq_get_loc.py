6276d2278ff3dbae7e55a7bc973166fc6cf6f9db
Workstation <wpwupingwp@outlook.com>
2018-11-19 11:50:02 +0800

    use dict to reduce query

--- qq_get_loc.py
+++ qq_get_loc.py
@@ -3,28 +3,30 @@
 import requests
 import json
 from time import sleep
+from sys import argv
 
 address = list()
-with open('./test.csv', 'r') as raw:
-#with open('./test.csv', 'r') as raw:
+with open(argv[1], 'r') as raw:
     for line in raw:
-        address.append(line.strip())
-address_dict = {i: False for i in address}
+        address.append(line.strip().split(','))
+address_dict = {i[1]: False for i in address}
 result = dict()
 for index, i in enumerate(address):
     print(index, i)
-    r = requests.get('http://apis.map.qq.com/ws/geocoder/v1/',
-                     params={'address': i,
+    r = requests.get('https://apis.map.qq.com/ws/geocoder/v1/',
+#                     proxies={'http': '127.0.0.1:1080'},
+                     params={'address': i[1],
                              'output': 'json',
                              'key': '2KHBZ-BTQAK-ADUJH-AXMTA-5H5L3-CPFI6'})
 
     if r.status_code == 200:
-        result[i] = r.json()
-        address_dict[i] = True
+        print(r.content)
+        result[i[0]] = r.json()
+        address_dict[i[1]] = True
     else:
-        print(r.status_code, i, r.text)
+        print(r.status_code, i, r.text.decode('utf-8'))
     sleep(0.22)
-with open('address_dict.json', 'w') as out:
+with open(argv[1]+'_address_dict.json', 'w') as out:
     json.dump(address_dict, out)
-with open('result.0424.json', 'w') as out:
+with open(argv[1]+'.json', 'w') as out:
     json.dump(result, out)
