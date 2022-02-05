628c2eb8822f34b18f3ec5de3b78479024db132d
Ping Wu <wpwupingwp@outlook.com>
2022-02-05 20:59:28 +0800

    rewrite done

--- google_get_loc.py
+++ google_get_loc.py
@@ -3,6 +3,7 @@
 import json
 from sys import argv
 from functools import lru_cache
+from pathlib import Path
 
 import requests
 
@@ -13,14 +14,14 @@ key = read_key()['google']
 proxy = {'http': '127.0.0.1:1080'}
 
 
-def get_query_list(list_file: Path) -> list:
-# get query list
-# Format:
-# Country,Province,City,Detail
-    address = []
+def get_address_list(list_file: Path) -> list:
+    # get query list
+    # Format:
+    # Country,Province,City,Detail
+    address_list = []
     with open(argv[1], 'r', encoding='utf8') as _:
         for line in _:
-            address.append(line.strip())
+            address_list.append(line.strip())
     return address_list
 
 
@@ -51,9 +52,13 @@ def google(query: str, key: str) -> 'json':
 
 
 def main():
-    for index, addr_str in enumerate(address):
-        print(index, addr_str)
-        addr_list = addr_str.split(',')
+    input_file = Path(argv[1])
+    out_file = input_file.with_suffix('.json')
+    all_result = list()
+    address_list = get_address_list(Path(argv[1]))
+    for index, address in enumerate(address_list):
+        print(index, address)
+        addr_list = address.split(',')
         query = ','.join(addr_list)
         js = google(query, key)
         while js['status'] != 'OK' and len(addr_list) != 0:
@@ -61,27 +66,16 @@ def main():
             query = ','.join(addr_list)
             print('\t', query)
             js = google(query, key)
-        if js['status'] == 'OK':
-            result = [addr_str, query, js]
-        else:
-            result = [addr_str, '', '']
-            continue
+            if js['status'] == 'OK':
+                result = [address, query, js]
+                break
+            else:
+                result = [address, '', '']
+        all_result.append(result)
+    with open(out_file, 'w', encoding='utf-8') as out:
+        json.dump(result, out)
+    return
 
-for index, address_str in enumerate(list(address_dict.keys())):
-    addr_list = address_str.split(',')
-    query = ','.join(addr_list)
-    print('{} of {}, {}'.format(index, total, query))
-    js = google(query, place_key, geocode_key)
-    while js['status'] != 'OK' and len(addr_list) != 0:
-        addr_list.pop()
-        query = ','.join(addr_list)
-        print('\t', query)
-        js = google(query, place_key, geocode_key)
-    if js['status'] == 'OK':
-        result = [address_str, query, js]
-    else:
-        continue
-    print('\t', result[2]['status'])
-    json.dump(result, out)
-    out.write('\n')
-    out.flush()
+
+if __name__ == '__main__':
+    main()
