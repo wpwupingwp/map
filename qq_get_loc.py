27db6ccc7fd419f48974585ac5c7708e5b99d1eb
Ping Wu <wpwupingwp@outlook.com>
2022-02-05 20:10:29 +0800

    qq loc search have distance limit, give up

--- qq_get_loc.py
+++ qq_get_loc.py
@@ -8,23 +8,26 @@ from sys import argv
 from read_key import read_key
 
 key = read_key()['qq']
+decode_url = 'https://apis.map.qq.com/ws/geocoder/v1/'
+search_url = 'https://apis.map.qq.com/ws/place/v1/search'
 address = list()
-with open(argv[1], 'r') as raw:
+# each line for one address, must be utf-8
+with open(argv[1], 'r', encoding='utf-8') as raw:
     for line in raw:
-        address.append(line.strip().split('\t'))
-address_dict = {i[1]: False for i in address}
+        address.append(line.strip())
+address_dict = {i: False for i in address}
 result = dict()
 for index, i in enumerate(address):
     print(index, i)
-    r = requests.get('https://apis.map.qq.com/ws/geocoder/v1/',
+    r = requests.get(decode_url, params={'address': i, 'output': 'json',
+                                         'key': key})
                      # proxies={'http': '127.0.0.1:1080'},
-                     params={'address': i[1], 'output': 'json', 'key': key})
     if r.status_code == 200:
         print(r.content)
-        result[i[0]] = r.json()
-        address_dict[i[1]] = True
+        result[i] = r.json()
+        address_dict[i] = True
     else:
-        print(r.status_code, i, r.text)
+        print(r.status_code, i, r.text.decode('utf8'))
     # five times per second
     sleep(0.22)
 with open(argv[1]+'_address_dict.json', 'w') as out:
