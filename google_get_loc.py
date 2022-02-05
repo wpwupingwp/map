9c194709ecc39802a027f74be1364259737f1d33
Ping Wu <wpwupingwp@outlook.com>
2022-02-05 20:52:19 +0800

    rewrite google_get_loc

--- google_get_loc.py
+++ google_get_loc.py
@@ -1,20 +1,40 @@
 #!/usr/bin/python3
 
-import requests
 import json
-import re
 from sys import argv
+from functools import lru_cache
+
+import requests
+
+from read_key import read_key
 
 
-def google(query, place_key, geocode_key):
+key = read_key()['google']
+proxy = {'http': '127.0.0.1:1080'}
+
+
+def get_query_list(list_file: Path) -> list:
+# get query list
+# Format:
+# Country,Province,City,Detail
+    address = []
+    with open(argv[1], 'r', encoding='utf8') as _:
+        for line in _:
+            address.append(line.strip())
+    return address_list
+
+
+@lru_cache(maxsize=0)
+def google(query: str, key: str) -> 'json':
     """
-    Input: query, place_key, geocode_key
+    Input: query, key
     Output: json()
     """
     place_url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json'
     geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json'
-    r = requests.get(place_url, params={'input': query, 'language': 'zh-CN',
-                                        'key': place_key})
+    r = requests.get(place_url,
+                     params={'input': query, 'language': 'zh-CN', 'key': key},
+                     proxies=proxy)
     if r.status_code == 200:
         autocomplete = r.json()
     else:
@@ -22,29 +42,31 @@ def google(query, place_key, geocode_key):
     if autocomplete['status'] == 'OK':
         place_id = autocomplete['predictions'][0]['place_id']
         r2 = requests.get(geocode_url, params={'place_id': place_id,
-                                               'key': geocode_key})
+                                               'key': key})
     else:
         r2 = requests.get(geocode_url, params={'address': query,
-                                               'key': geocode_key})
+                                               'key': key})
     js = r2.json()
     return js
 
 
-# get key
-with open('key', 'r') as key:
-    place_key = key.readline().strip().split(' ')[1]
-    geocode_key = key.readline().strip().split(' ')[1]
-# get query list
-# Format:
-# Name,Country,Province,City,Detail
-with open(argv[1], 'r') as _:
-    raw = _.readlines()
-    raw2 = [i.strip().split(',') for i in raw]
-    name_address = [[i[0], ','.join(i[1:])] for i in raw2]
-address_dict = {i[1]: 0 for i in name_address}
-total = len(address_dict)
+def main():
+    for index, addr_str in enumerate(address):
+        print(index, addr_str)
+        addr_list = addr_str.split(',')
+        query = ','.join(addr_list)
+        js = google(query, key)
+        while js['status'] != 'OK' and len(addr_list) != 0:
+            addr_list.pop()
+            query = ','.join(addr_list)
+            print('\t', query)
+            js = google(query, key)
+        if js['status'] == 'OK':
+            result = [addr_str, query, js]
+        else:
+            result = [addr_str, '', '']
+            continue
 
-out = open(argv[1]+'.json', 'w')
 for index, address_str in enumerate(list(address_dict.keys())):
     addr_list = address_str.split(',')
     query = ','.join(addr_list)
